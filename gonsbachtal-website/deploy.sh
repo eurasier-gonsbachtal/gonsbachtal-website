05cdac444a91e6b54f7c02542465717bcd5a13af
jbohland <jbohland@students.uni-mainz.de>
2022-08-22 13:39:55 +0200

    404 error

--- gonsbachtal-website/deploy.sh
+++ gonsbachtal-website/deploy.sh
@@ -5,6 +5,8 @@ set -e
 npm run build
 # navigate into the build output directory
 cd dist
+
+cp index.html 404.html
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
